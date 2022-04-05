47feba894d3f684d91d7d48fd9a9b7d06839ab66
almas <almaskon@gmail.com>
2022-04-05 19:12:27 +0600

    added userlist update of online users. fix bug of ScrollPane

--- src/main/java/com/homework4/server/handler/ClientHandler.java
+++ src/main/java/com/homework4/server/handler/ClientHandler.java
@@ -6,7 +6,10 @@ import com.homework4.server.authentication.AuthenticationService;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientHandler {
     private static final String AUTH_CMD_PREFIX = "/auth"; // + login + password
@@ -17,10 +20,12 @@ public class ClientHandler {
     private static final String PRIVATE_MSG_CMD_PREFIX = "/pMsg"; // + msg
     private static final String STOP_SERVER_CMD_PREFIX = "/stop";
     private static final String END_CLIENT_CMD_PREFIX = "/end";
+    private static final String GET_CLIENTS_CMD_PREFIX = "/getCls";
 
     private ChatServer myServer;
     private Socket clientSocket;
     private DataOutputStream out;
+    private ObjectOutputStream objOut;
     private DataInputStream in;
     private String username;
 
@@ -40,6 +45,12 @@ public class ClientHandler {
                 readMessage();
             } catch (IOException e) {
                 e.printStackTrace();
+                myServer.unSubscribe(this);
+                try {
+                    myServer.broadcastClientDisconnected(this);
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
             }
         }).start();
     }
@@ -52,7 +63,6 @@ public class ClientHandler {
                 if (isSuccessAuth) {
                     break;
                 }
-
             } else {
                 out.writeUTF(AUTHERR_CMD_PREFIX + " Ошибка аутентификации");
                 System.out.println("Неудачная попытка аутентификации");
@@ -77,15 +87,19 @@ public class ClientHandler {
                 out.writeUTF(AUTHERR_CMD_PREFIX + " Логин уже используется");
                 return false;
             }
-
             out.writeUTF(AUTHOK_CMD_PREFIX + " " + username);
-            myServer.subscribe(this);
-            System.out.println("Пользователь " + username + " подключился к чату");
+            connectUser(username);
             return true;
         } else {
             out.writeUTF(AUTHERR_CMD_PREFIX + " Логин или пароль не соответствуют действительности");
             return false;
         }
+    }
+
+    private void connectUser(String username) throws IOException {
+        myServer.subscribe(this);
+        System.out.println("Пользователь " + username + " подключился к чату");
+        myServer.broadcastClients(this);
     }
 
     private void readMessage() throws IOException {
@@ -99,15 +113,26 @@ public class ClientHandler {
                 return;
             } else if (message.startsWith(PRIVATE_MSG_CMD_PREFIX)) {
                 myServer.privateMessage(message, this);
-            } else if (message.startsWith(CLIENT_MSG_CMD_PREFIX)) {
+            } else {
                 myServer.broadcastMessage(message, this);
             }
-
         }
     }
 
     public void sendMessage(String sender, String message) throws IOException {
-        out.writeUTF(String.format("%s %s", sender, message));
+        if (sender != null) {
+            sendClientMessage(sender, message);
+        } else {
+            sendServerMessage(message);
+        }
+    }
+
+    public void sendServerMessage(String message) throws IOException {
+        out.writeUTF(String.format("%s %s", SERVER_MSG_CMD_PREFIX, message));
+    }
+
+    public void sendClientMessage(String sender, String message) throws IOException {
+        out.writeUTF(String.format("%s %s %s", CLIENT_MSG_CMD_PREFIX, sender, message));
     }
 
     public String getUsername() {
@@ -116,5 +141,16 @@ public class ClientHandler {
 
     public void noUsername(String wrongUser) throws IOException {
         out.writeUTF("No such user " + wrongUser);
+    }
+
+    public void sendClientsList(List<ClientHandler> clients) throws IOException {
+        String msg = String.format("%s %s", GET_CLIENTS_CMD_PREFIX, clients.toString());
+        out.writeUTF(msg);
+        System.out.println(msg);
+    }
+
+    @Override
+    public String toString() {
+        return username;
     }
 }
